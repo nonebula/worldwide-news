4e8058772022a6bfbfeef2bf1896f21e181db93f
Joe Lloyd Higgs <joe@Joes-MacBook-Air.local>
2024-05-13 14:01:56 +0100

    File structure

---
+++ src/pages/categories/Categories.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './Categories.module.css'
+const Categories = () => {
+  return (
+    <div className={styles.container}>
+      Categories
+    </div>
+  )
+}
+
+export default Categories
