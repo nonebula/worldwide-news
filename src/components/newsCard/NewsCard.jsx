004a285798d8ef129b31ddfe70d605e87c83a64a
Joe Lloyd Higgs <joe@Joes-MacBook-Air.local>
2024-05-14 09:33:14 +0100

    News card and card styling

--- src/components/newsCard/NewsCard.jsx
+++ src/components/newsCard/NewsCard.jsx
@@ -1,7 +1,21 @@
 import React from "react";
-
-const NewsCard = () => {
-  return <div></div>;
+import styles from "./NewsCard.module.css";
+// add random.png news image
+const NewsCard = ({ title, url, urlToImage, content }) => {
+  return (
+    <div className={styles.card}>
+      <img src={urlToImage ? urlToImage : random} alt="News Article" />
+      <div className={styles.cardDetail}>
+        <h3>{title}</h3>
+        <p>{content}</p>
+        <div className={styles.a}>
+          <a href={url} rel="noreferrer" target="blank" className={styles.link}>
+            Detail
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NewsCard;
