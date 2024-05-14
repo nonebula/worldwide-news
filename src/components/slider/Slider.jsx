dba97669211c10ffbefb0b88091e23719f399e20
Joe Lloyd Higgs <joe@Joes-MacBook-Air.local>
2024-05-14 11:28:37 +0100

    Slider logic & styles

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { useState } from "react";
+import styles from "./Slider.module.css";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
 
-const Slider = () => {
-  return <div></div>;
+const Slider = ({ sliderNews }) => {
+  const [current, setCurrent] = useState(0);
+  const length = sliderNews.length;
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+
+  if (!Array.isArray(sliderNews) || sliderNews.length <= 0) {
+    return null;
+  }
+
+  return (
+    <div className={styles.slider}>
+      <FaArrowAltCircleLeft className={styles.left} onClick={prevSlide} />
+      <FaArrowAltCircleRight className={styles.right} onClick={nextSlide} />
+      {sliderNews.map((item, index) => {
+        return (
+          <div key={index}>
+            {index === current && (
+              <div className={styles.container}>
+                <img src={item.urlToImage} alt="img" className={styles.image} />
+                <h3>{item.title}</h3>
+                <div className={styles.link}>
+                  <a href={item.url} target="blank" rel="noreferrer">
+                    Detail
+                  </a>
+                </div>
+              </div>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Slider;
